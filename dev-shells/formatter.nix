aba88aec947c865f5406eb5c6fcafa1e483137c3
Lin Jian <me@linj.tech>
2026-05-22 05:45:08 +0800

    formatter: avoid running treefmt twice in `nix flake check`
    Previously, `nix flake check` run treefmt twice via:
    - checks.<system>.treefmt
    - checks.<system>.pre-commit
    
    With this patch, we prefer to run treefmt together with pre-commit.

--- dev-shells/formatter.nix
+++ dev-shells/formatter.nix
@@ -3,17 +3,21 @@
 {
   imports = [ inputs.treefmt-nix.flakeModule ];
 
-  perSystem = {
-    treefmt = {
-      programs = {
-        nixfmt.enable = true;
-        deadnix.enable = true;
-        nixf-diagnose.enable = true;
+  perSystem =
+    { config, ... }:
+    {
+      treefmt = {
+        flakeCheck = !(config.pre-commit.check.enable && config.pre-commit.settings.hooks.treefmt.enable);
+
+        programs = {
+          nixfmt.enable = true;
+          deadnix.enable = true;
+          nixf-diagnose.enable = true;
+        };
+
+        programs.yamlfmt.enable = true;
+
+        programs.keep-sorted.enable = true;
       };
-
-      programs.yamlfmt.enable = true;
-
-      programs.keep-sorted.enable = true;
     };
-  };
 }
