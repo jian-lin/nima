0c427795fe64337aba40be0dfa01851e120db79d
Lin Jian <me@linj.tech>
2026-05-28 12:38:04 +0800

    doc: add doc and example for the default value of elispFile

--- examples/nima-modules/simple/features/prelude.nix
+++ examples/nima-modules/simple/features/prelude.nix
@@ -1,5 +1,5 @@
 {
   # we can use `elispFile` when we do not need to access the nix world in our elisp config
-  elispFile = ./prelude.el;
+  elispFile = ./prelude.el; # this is the default value, so it can be commented out / removed
   order = -100; # make this elisp config come first in the generated default.el
 }
